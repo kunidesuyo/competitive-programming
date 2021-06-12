38bd8f4f965210c703b579979a82842270a2c04c
kunidesuyo <kunidesuyo1234@gmail.com>
2021-06-12 20:40:29 +0900

    bootcamphard途中

--- ABC/151~200/153/E.cpp
+++ ABC/151~200/153/E.cpp
@@ -2,45 +2,59 @@
 #include <cmath>
 
 using ll = long long;
-const ll INF = __LONG_LONG_MAX__;
 
 using namespace std;
+
+template<class T> inline bool chmin(T& a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+    }
+template<class T> inline bool chmax(T& a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
 
 int main() {
     int h, n;
     cin >> h >> n;
-    int a[n+1], b[n+1];
+    int a[n], b[n];
     for(int i = 0; i < n; i++) cin >> a[i] >> b[i];
 
-    int dp[n+1][h+1];
+    int dp[n][h+1];
     const int inf = 1000000000;
     
-
-    for(int i = 0; i < n+1; i++) {
-        for(int j = 0; j < h+1; j++) {
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j <= h; j++) {
             dp[i][j] = inf;
         }
     }
-    dp[0][h] = 0;
+    for(int i = 0; i < n; i++) {
+        dp[i][0] = 0;
+    }
 
     for(int i = 0; i < n; i++) {
-        for(int j = h; j >= 0; j--) {
-            dp[i+1][j] = min(dp[i+1][j], dp[i][j]);
-            dp[i][max(0, j-a[i])] = min(dp[i][max(0, j-a[i])], dp[i][j] + b[i]);
+        for(int j = 0; j <= h; j++) {
+            if(j - a[i+1] >= 0) chmin(dp[i+1][j], dp[i][j-a[i+1]] + b[i+1]);
+            chmin(dp[i+1][j], dp[i][j]);
         }
     }
 
-    for(int i = 0; i < n+1; i++) {
-        for(int j = 0; j < h+1; j++) {
-            if(dp[i][j] == inf) {
-                cout << "INF ";
-            } else {
-                cout << dp[i][j] << " ";
-            }
+    for(int i = 0; i <= n; i++) {
+        for(int j = 0; j <= h; j++) {
+            if(dp[i][j] == inf) cout << "INF ";
+            else printf("%3d ", dp[i][j]);
         }
         cout << "\n";
     }
-    
+
+    cout << dp[n][h] << endl;
+       
 
     return 0;
 }
