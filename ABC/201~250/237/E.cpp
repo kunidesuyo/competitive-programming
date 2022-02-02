9024f760292f39d2e1e1b6ae2be0633dc3f7a608
kunidesuyo <kunidesuyo1234@gmail.com>
2022-02-02 21:31:50 +0900

    ABC237E

--- ABC/201~250/237/E.cpp
+++ ABC/201~250/237/E.cpp
@@ -5,23 +5,50 @@
 
 using ll = long long;
 const ll INF = 1LL<<60;
-const int inf = INT_MAX / 2;
 
 using namespace std;
 
+typedef pair<ll, ll> P;
+
 int main() {
-    int n,m;
+    ll n,m;
     cin >> n >> m;
-    int h[n];
-    for(int i = 0; i < n; i++) cin >> h[i];
-    vector<int> g[n];
-    for(int i = 0; i < m; i++) {
-        int u, v;
+    ll h[n];
+    for(ll i = 0; i < n; i++) cin >> h[i];
+    vector<ll> g[n];
+    for(ll i = 0; i < m; i++) {
+        ll u, v;
         cin >> u >> v;
         u--; v--;
         g[u].push_back(v);
         g[v].push_back(u);
     }
+
+    priority_queue<P, vector<P>, greater<P>> q;
+    vector<ll> dist(n, INF);
+    dist[0] = 0;
+    q.emplace(0,0);
+
+    while(!q.empty()) {
+        P dv = q.top(); q.pop();
+        ll d = dv.first;
+        ll v = dv.second;
+        if(dist[v] != d) continue;
+        for(auto u : g[v]) {
+            ll cost = max(0LL, h[u]-h[v]);
+            ll nd = d + cost;
+            if(nd >= dist[u]) continue;
+            dist[u] = nd;
+            q.emplace(nd, u);
+        }
+    }
+
+    ll ans = 0;
+    for(ll i = 0; i < n; i++) {
+        ll now = h[0] - h[i] - dist[i];
+        ans = max(ans, now);
+    }
+    cout << ans << endl;
     /*for(auto x : g) {
         for(auto y : x) {
             cout << y << " ";
@@ -29,16 +56,18 @@ int main() {
         cout << "\n";
     }*/
 
-    queue<int> q;
-    vector<int> fan(n, -inf);
+
+
+    /*queue<ll> q;
+    vector<ll> fan(n, -inf);
     q.push(0);
     fan[0] = 0;
     
     while(!q.empty()) {
-        int now = q.front();
+        ll now = q.front();
         q.pop();
         for(auto next : g[now]) {
-            int next_f = fan[now];
+            ll next_f = fan[now];
             if(h[now] < h[next]) {
                 next_f -= 2 * abs(h[now] - h[next]);
             } else if(h[now] > h[next]) {
@@ -51,11 +80,11 @@ int main() {
         }
     }
 
-    int ans = -inf;
+    ll ans = -inf;
     for(auto x:fan) {
         ans = max(ans, x);
     }
-    cout << ans << endl;
+    cout << ans << endl;*/
 
     return 0;
 }
