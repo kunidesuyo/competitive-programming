8dcc1d44394223b47d1a202666ae8fc7451bb7f0
kuni <kunidesuyo1234@gmail.com>
2024-09-12 23:02:44 +0900

    251B

--- ABC/251~300/251/B.cpp
+++ ABC/251~300/251/B.cpp
@@ -10,7 +10,23 @@ const int inf = INT_MAX / 2;
 using namespace std;
 
 int main() {
-    
+    int n, w;
+    cin >> n >> w;
+    int a[n+2];
+    for(int i = 0; i < n; i++) cin >> a[i];
+    a[n] = 0;
+    a[n+1] = 0;
+
+    set<int> ans;
+
+    for(int j = 0; j < n; j++) {
+        for(int k = j+1; k < n+1; k++) {
+            for(int l = k+1; l < n+2; l++) {
+                if(a[j]+a[k]+a[l] <= w) ans.insert(a[j]+a[k]+a[l]);
+            }
+        }
+    }
+    cout << ans.size() << endl;
 
     return 0;
 }
