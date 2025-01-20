f47cbd72103a3a60d2ab8af2d1a4d971251185ec
kuni <kunidesuyo1234@gmail.com>
2025-01-20 19:32:30 +0900

    302A

---
+++ ABC/301~350/302/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+#define all(x) (x).begin(), (x).end()
+
+using ll = long long;
+const ll INF = 1LL << 60;
+const int inf = INT_MAX / 2;
+
+using namespace std;
+
+int main()
+{
+  ll a, b;
+  cin >> a >> b;
+  if (a % b == 0)
+  {
+    cout << a / b << endl;
+  }
+  else
+  {
+    cout << a / b + 1 << endl;
+  }
+
+  return 0;
+}
