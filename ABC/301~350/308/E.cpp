e6a1bf4ec60b56f403dbea7aa1b096f572b35dae
kuni <kunidesuyo1234@gmail.com>
2025-02-04 22:39:05 +0900

    308E

--- ABC/301~350/308/E.cpp
+++ ABC/301~350/308/E.cpp
@@ -28,46 +28,66 @@ int main() {
   string s;
   cin >> s;
 
-  vector<vector<int>> m(3, vector<int>(n + 1)), x(3, vector<int>(n + 1));
-  m[0][0] = 0;
-  m[1][0] = 0;
-  m[2][0] = 0;
-  x[0][0] = 0;
-  x[1][0] = 0;
-  x[2][0] = 0;
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < 3; j++) {
-      m[j][i + 1] = m[j][i];
-    }
-    if (s[i] == 'M') {
-      m[a[i]][i + 1]++;
-    }
-  }
-
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < 3; j++) {
-      x[j][i + 1] = x[j][i];
-    }
-    if (s[i] == 'X') {
-      x[a[i]][i + 1]++;
-    }
-  }
-
   ll ans = 0;
+  vector<int> m(3), x(3);
+  for (int i = 0; i < n; i++) {
+    if (s[i] == 'X') x[a[i]]++;
+  }
 
   for (int j = 0; j < n; j++) {
     if (s[j] == 'E') {
       for (int i = 0; i < 3; i++) {
         for (int k = 0; k < 3; k++) {
-          ans +=
-              c(i, a[j], k) * ((m[i][j] - m[i][0]) * (x[k][n] - x[k][j + 1]));
+          ll now = (ll)m[i] * x[k];
+          ans += now * c(i, a[j], k);
         }
       }
     }
+    if (s[j] == 'M') m[a[j]]++;
+    if (s[j] == 'X') x[a[j]]--;
   }
-
   cout << ans << endl;
+
+  // vector<vector<int>> m(3, vector<int>(n + 1)), x(3, vector<int>(n + 1));
+  // m[0][0] = 0;
+  // m[1][0] = 0;
+  // m[2][0] = 0;
+  // x[0][n] = 0;
+  // x[1][n] = 0;
+  // x[2][n] = 0;
+
+  // for (int i = 0; i < n; i++) {
+  //   for (int j = 0; j < 3; j++) {
+  //     m[j][i + 1] = m[j][i];
+  //   }
+  //   if (s[i] == 'M') {
+  //     m[a[i]][i + 1]++;
+  //   }
+  // }
+
+  // for (int i = n - 1; i >= 0; i--) {
+  //   for (int j = 0; j < 3; j++) {
+  //     x[j][i] = x[j][i + 1];
+  //   }
+  //   // cout << i << endl;
+  //   if (s[i] == 'X') {
+  //     x[a[i]][i]++;
+  //   }
+  // }
+
+  // ll ans = 0;
+
+  // for (int j = 0; j < n; j++) {
+  //   if (s[j] == 'E') {
+  //     for (int i = 0; i < 3; i++) {
+  //       for (int k = 0; k < 3; k++) {
+  //         ans += c(i, a[j], k) * (m[i][j] * x[k][j + 1]);
+  //       }
+  //     }
+  //   }
+  // }
+
+  // cout << ans << endl;
 
   return 0;
 }
