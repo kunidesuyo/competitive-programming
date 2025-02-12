3a9c81a4dc0f57acfcef74e0ca81136c8cb8a6c1
kuni <kunidesuyo1234@gmail.com>
2025-02-13 07:56:19 +0900

    update template

--- templates/atcoder.cpp
+++ templates/atcoder.cpp
@@ -4,10 +4,10 @@ using namespace std;
 using namespace atcoder;
 #include <cmath>
 
-#define all(x) (x).begin(),(x).end()
+#define all(x) (x).begin(), (x).end()
 
 using ll = long long;
-const ll INF = 1LL<<60;
+const ll INF = 1LL << 60;
 const int inf = INT_MAX / 2;
 
 int main() {
