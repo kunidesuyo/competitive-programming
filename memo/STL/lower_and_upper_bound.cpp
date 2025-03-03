4cf3e7053c9b06d776bc260a594bd4103c77e480
kuni <kunidesuyo1234@gmail.com>
2025-03-03 19:28:14 +0900

    update lower_and_upper_bound

--- memo/STL/lower_and_upper_bound.cpp
+++ memo/STL/lower_and_upper_bound.cpp
@@ -11,32 +11,48 @@ const int inf = INT_MAX / 2;
 using namespace std;
 
 int main() {
-  //ソート済配列
+  // ソート済配列
   //     index: 0  1  2  3  4
   vector<int> x{1, 3, 5, 7, 9};
 
   // lower_boundは指定された要素の値*以上*の値が現れる最初のイテレータを取得する。
-  int i_a = lower_bound(x.begin(), x.end(), 4) - x.begin();  // 2
-  int i_b = lower_bound(x.begin(), x.end(), 5) - x.begin();  // 2
-  cout << i_a << endl;
-  cout << i_b << endl;
+  vector<int> lb_index;
+  lb_index.push_back(lower_bound(x.begin(), x.end(), 0) - x.begin());  // 0
+  lb_index.push_back(lower_bound(x.begin(), x.end(), 4) - x.begin());  // 2
+  lb_index.push_back(lower_bound(x.begin(), x.end(), 5) - x.begin());  // 2
+  lb_index.push_back(lower_bound(x.begin(), x.end(), 10) - x.begin());  // 5 = x.end()
+  for (auto i : lb_index) {
+    cout << i << endl;
+  }
   // 直接値を取得する
-  int a = *lower_bound(x.begin(), x.end(), 4);  // 5
-  int b = *lower_bound(x.begin(), x.end(), 5);  // 5
-  cout << a << endl;
-  cout << b << endl;
-  
+  vector<int> lb_value;
+  lb_value.push_back(*lower_bound(x.begin(), x.end(), 0));  // 1
+  lb_value.push_back(*lower_bound(x.begin(), x.end(), 4));  // 5
+  lb_value.push_back(*lower_bound(x.begin(), x.end(), 5));  // 5
+  // lb_value.push_back(*lower_bound(x.begin(), x.end(), 10));  // ?
+  for (auto v : lb_value) {
+    cout << v << endl;
+  }
 
   // upper_boundは指定された要素の値より*大きい*値が現れる最初のイテレータを取得する。
-  int i_c = upper_bound(x.begin(), x.end(), 4) - x.begin();  // 2
-  int i_d = upper_bound(x.begin(), x.end(), 5) - x.begin();  // 3
-  cout << i_c << endl;
-  cout << i_d << endl;
+  vector<int> ub_index;
+  ub_index.push_back(upper_bound(x.begin(), x.end(), 0) - x.begin());  // 0
+  ub_index.push_back(upper_bound(x.begin(), x.end(), 4) - x.begin());  // 2
+  ub_index.push_back(upper_bound(x.begin(), x.end(), 5) - x.begin());  // 3
+  ub_index.push_back(upper_bound(x.begin(), x.end(), 10) - x.begin());  // 5 = x.end()
+  for (auto i : ub_index) {
+    cout << i << endl;
+  }
+
   // 直接値を取得する
-  int c = *upper_bound(x.begin(), x.end(), 4);  // 5
-  int d = *upper_bound(x.begin(), x.end(), 5);  // 7
-  cout << c << endl;
-  cout << d << endl;
+  vector<int> ub_value;
+  ub_value.push_back(*upper_bound(x.begin(), x.end(), 0));  // 1
+  ub_value.push_back(*upper_bound(x.begin(), x.end(), 4));  // 5
+  ub_value.push_back(*upper_bound(x.begin(), x.end(), 5));  // 7
+  // ub_value.push_back(*upper_bound(x.begin(), x.end(), 10));  // 7
+  for (auto v : ub_value) {
+    cout << v << endl;
+  }
 
   return 0;
 }
