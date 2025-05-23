605cfb9d06f5d50c4611cd358103ff773eb1a813
kuni <kunidesuyo1234@gmail.com>
2025-05-23 20:37:21 +0900

    314c

--- src/atcoder/abc/abc314/c/abc314_c.cpp
+++ src/atcoder/abc/abc314/c/abc314_c.cpp
@@ -11,6 +11,28 @@ const ll INF = 1LL << 60;
 const int inf = INT_MAX / 2;
 
 int main() {
-  
+  int n, m;
+  cin >> n >> m;
+  string s;
+  cin >> s;
+  vector<int> c(n);
+  for (int i = 0; i < n; i++) cin >> c[i];
+  vector<deque<char>> d(m);
+  for (int i = 0; i < n; i++) {
+    d[c[i] - 1].push_back(s[i]);
+  }
+
+  for (int i = 0; i < m; i++) {
+    char x = d[i].back();
+    d[i].pop_back();
+    d[i].push_front(x);
+  }
+
+  for (int i = 0; i < n; i++) {
+    char x = d[c[i] - 1].front();
+    cout << x;
+    d[c[i] - 1].pop_front();
+  }
+  cout << "\n";
   return 0;
 }
