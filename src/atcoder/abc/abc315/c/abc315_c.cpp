e9cc4cbe62113b2d8384452deb2775735c028058
kuni <kunidesuyo1234@gmail.com>
2025-05-24 20:14:05 +0900

    315c

--- src/atcoder/abc/abc315/c/abc315_c.cpp
+++ src/atcoder/abc/abc315/c/abc315_c.cpp
@@ -11,6 +11,40 @@ const ll INF = 1LL << 60;
 const int inf = INT_MAX / 2;
 
 int main() {
-  
+  int n;
+  cin >> n;
+  vector<pair<int, int>> fs;
+  vector<pair<int, int>> sf;
+  for (int i = 0; i < n; i++) {
+    int f, s;
+    cin >> f >> s;
+    fs.push_back(pair(f, s));
+    sf.push_back(pair(s, f));
+  }
+
+  sort(all(sf));
+  sort(all(fs));
+
+  // same
+  int same = -1;
+  for (int i = 0; i < n - 1; i++) {
+    if (fs[i].first == fs[i + 1].first) {
+      int sat = max(fs[i].second, fs[i + 1].second) +
+                min(fs[i].second, fs[i + 1].second) / 2;
+      same = max(same, sat);
+    }
+  }
+
+  // different
+  int diff = -1;
+  pair<int, int> ice = sf[n - 1];
+  for (int i = n - 2; i >= 0; i--) {
+    if (sf[i].second != ice.second) {
+      diff = max(diff, sf[i].first + ice.first);
+    }
+  }
+
+  cout << max(same, diff) << endl;
+
   return 0;
 }
