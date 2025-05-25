43b10dc59bec80cdb3a4a6b945ac22677b5e3bd2
kuni <kunidesuyo1234@gmail.com>
2025-05-25 12:21:33 +0900

    318c

--- src/atcoder/abc/abc318/c/abc318_c.cpp
+++ src/atcoder/abc/abc318/c/abc318_c.cpp
@@ -7,10 +7,38 @@ using namespace atcoder;
 #define all(x) (x).begin(), (x).end()
 
 using ll = long long;
-const ll INF = 1LL << 60;
-const int inf = INT_MAX / 2;
 
 int main() {
-  
+  ll n, d, p;
+  cin >> n >> d >> p;
+  vector<ll> f(n);
+  for (ll i = 0; i < n; i++) cin >> f[i];
+
+  sort(all(f), greater<ll>());
+
+  ll s = n / d;
+  // ll t = n % d;
+  vector<ll> ff;
+  for (ll i = 0; i < s; i++) {
+    ll sum = 0;
+    for (ll j = 0; j < d; j++) {
+      ll index = d * i + j;
+      sum += f[index];
+    }
+    ff.push_back(sum);
+  }
+  ll sum = 0;
+  for (ll i = s * d; i < n; i++) {
+    sum += f[i];
+  }
+  ff.push_back(sum);
+
+  ll ans = 0;
+  for (auto g : ff) {
+    // cout << g << endl;
+    ans += min(g, p);
+  }
+  cout << ans << endl;
+
   return 0;
 }
