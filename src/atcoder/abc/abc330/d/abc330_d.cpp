9ab2680658e3a0c0504910b06efadd35fd573007
kuni <kunidesuyo1234@gmail.com>
2025-06-01 00:19:27 +0900

    330d

--- src/atcoder/abc/abc330/d/abc330_d.cpp
+++ src/atcoder/abc/abc330/d/abc330_d.cpp
@@ -7,10 +7,31 @@ using namespace atcoder;
 #define all(x) (x).begin(), (x).end()
 
 using ll = long long;
-const ll INF = 1LL << 60;
-const int inf = INT_MAX / 2;
 
 int main() {
-  
+  ll n;
+  cin >> n;
+  string s[n];
+  for (ll i = 0; i < n; i++) cin >> s[i];
+  vector<ll> retu(n, 0);
+  for (ll i = 0; i < n; i++) {
+    for (ll j = 0; j < n; j++) {
+      if (s[j][i] == 'o') retu[i]++;
+    }
+  }
+
+  ll ans = 0;
+  for (ll i = 0; i < n; i++) {
+    ll sum = 0;
+    for (ll j = 0; j < n; j++) {
+      if (s[i][j] == 'o') sum++;
+    }
+    sum--;
+    if (sum <= 0) continue;
+    for (ll j = 0; j < n; j++) {
+      if (s[i][j] == 'o') ans += (retu[j] - 1) * sum;
+    }
+  }
+  cout << ans << endl;
   return 0;
 }
